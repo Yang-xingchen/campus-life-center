563727ce2ecff64bf9eafd37a7a4d4abd67b9cf6
1459177541 <1459177541@qq.com>
2021-01-07 20:37:13 +0800

    finish analysis page.

--- web/src/views/notice/components/Analysis.vue
+++ web/src/views/notice/components/Analysis.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-    {{ analysis }}
-    <a-divider><span class="divider">设置</span></a-divider>
-    <div class="setting">
+    <a-divider orientation="right">
+      <span
+        class="divider setting_title"
+        v-show="setting"
+        @click="setting = !setting"
+        >设置<a-icon type="up"
+      /></span>
+      <span
+        class="divider setting_title"
+        v-show="!setting"
+        @click="setting = !setting"
+        >设置<a-icon type="down"
+      /></span>
+    </a-divider>
+    <div class="setting" v-show="setting">
       显示方式:
       <a-radio-group v-model="showtype">
         <a-radio-button value="id">id</a-radio-button>
         <a-radio-button value="name">名称</a-radio-button>
+      </a-radio-group>
+      分隔符:
+      <a-radio-group v-model="divider">
+        <a-radio-button value=",">英文逗号</a-radio-button>
+        <a-radio-button value=" ">空格</a-radio-button>
       </a-radio-group>
     </div>
     <a-divider />
@@ -42,16 +59,24 @@
         >收起<a-icon type="up"
       /></span>
       <div class="publish_list" v-show="publish_show">
-        // todo
+        显示收到通知用户<a-switch v-model="publish_success" />
+        显示未收到通知用户<a-switch v-model="publish_fail" />
+        <div class="publish_content">{{ publish_show_account }}</div>
       </div>
     </div>
     <a-divider><span class="divider">用户状态</span></a-divider>
     <div class="accountOperation">
       读取比例:
+      <span v-if="account_show">显示已读 <a-switch v-model="ao_r"/></span>
+      <span v-if="account_show">显示未读 <a-switch v-model="ao_nr"/></span>
       <a-progress :percent="lookedPer" />
       置顶比例:
+      <span v-if="account_show">显示置顶 <a-switch v-model="ao_t"/></span>
+      <span v-if="account_show">显示未置顶 <a-switch v-model="ao_nt"/></span>
       <a-progress :percent="topPer" />
       完成比例:
+      <span v-if="account_show">显示完成 <a-switch v-model="ao_d"/></span>
+      <span v-if="account_show">显示未完成 <a-switch v-model="ao_nd"/></span>
       <a-progress :percent="delPer" />
       <div class="account_box">
         <span
@@ -67,13 +92,15 @@
           >收起<a-icon type="up"
         /></span>
         <div class="accountOper" v-show="account_show">
-          // todo
+          {{ operation_show_account }}
         </div>
       </div>
     </div>
-    <a-divider><span class="divider">待办状态</span></a-divider>
+    <a-divider><span class="divider">待办完成情况</span></a-divider>
     <div class="todo" v-for="t in analysis.accountTodos" :key="t.id">
       {{ t.value }}:
+      <span v-if="todo_show">显示完成<a-switch v-model="todo[t.id].f"/></span>
+      <span v-if="todo_show">显示未完成<a-switch v-model="todo[t.id].n"/></span>
       <a-progress :percent="todoFinPer(t.id)"></a-progress>
     </div>
     <div class="todo_box">
@@ -84,7 +111,7 @@
         >收起<a-icon type="up"
       /></span>
       <div class="todo_list" v-show="todo_show">
-        // todo
+        {{ account_todo_show }}
       </div>
     </div>
   </div>
@@ -129,15 +156,70 @@ export default {
         obj[n.id] ? "" : (obj[n.id] = true && c.push(n));
         return c;
       }, []);
+    },
+    publish_show_account() {
+      return this.pubilishList
+        .filter(
+          a =>
+            (this.publish_success && this.getAccountInfo(a.id) !== null) ||
+            (this.publish_fail && this.getAccountInfo(a.id) === null)
+        )
+        .map(a => a[this.showtype])
+        .join(this.divider);
+    },
+    operation_show_account() {
+      return this.analysis.accountNotice
+        .map(a => {
+          return { ...a };
+        })
+        .filter(
+          a =>
+            (this.ao_r && a.looked) ||
+            (this.ao_nr && !a.looked) ||
+            (this.ao_t && a.top) ||
+            (this.ao_nt && !a.top) ||
+            (this.ao_d && a.del) ||
+            (this.ao_nd && !a.del)
+        )
+        .map(
+          a => this.pubilishList.filter(p => p.id === a.aid)[0][this.showtype]
+        )
+        .join(this.divider);
+    },
+    account_todo_show() {
+      let list = [];
+      for (let k of this.analysis.accountTodos) {
+        if (
+          (this.todo[k.id].f && k.finish) ||
+          (this.todo[k.id].n && !k.finish)
+        ) {
+          list.push(k.aid);
+        }
+      }
+      list = [...new Set(list)];
+      return list
+        .map(a => this.pubilishList.filter(p => p.id === a)[0][this.showtype])
+        .join(this.divider);
     }
   },
   data() {
     return {
       analysis: { accountNotice: [], publishAccountList: [] },
+      setting: true,
       showtype: "id",
+      divider: ",",
       publish_show: false,
+      publish_success: false,
+      publish_fail: true,
       account_show: false,
-      todo_show: false
+      ao_r: false,
+      ao_nr: false,
+      ao_t: false,
+      ao_nt: false,
+      ao_d: false,
+      ao_nd: false,
+      todo_show: false,
+      todo: {}
     };
   },
   methods: {
@@ -181,12 +263,20 @@ export default {
   watch: {
     notice() {
       this.getAnalysis();
+    },
+    analysis() {
+      this.todo = Object.fromEntries(
+        [...this.analysis.accountTodos].map(t => [t.id, { f: false, n: false }])
+      );
     }
   }
 };
 </script>
 
 <style lang="less" scoped>
+.setting_title {
+  cursor: pointer;
+}
 .err {
   color: #ff8888;
 }
@@ -203,11 +293,17 @@ export default {
     display: flex;
     flex-wrap: wrap;
     align-content: flex-start;
-    .account {
-      margin: 3px 5px;
-      cursor: pointer;
+    .publish_content {
+      width: 100%;
+      word-break: keep-all;
+      padding-left: 30px;
     }
   }
+}
+.accountOper,
+.todo_list {
+  padding-top: 30px;
+  padding-left: 30px;
 }
 .account_box {
   min-height: 30px;
